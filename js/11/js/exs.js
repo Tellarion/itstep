985f4d068393e3a6b899e82084f1b896c8e87acd
Tellarion <tellariondev@gmail.com>
2021-05-14 02:31:43 +0300

    update

--- js/11/js/exs.js
+++ js/11/js/exs.js
@@ -45,11 +45,11 @@ function show_modal() {
 let get_width = $('#ex_3').width()
 let get_height = $('#ex_3').height()
 function set_position() {
-    let center_t = get_height / 2.2;
+    let center_t = get_height / 2.2
     if(get_width >= 2000) {
-        center_t = get_height / 2;
+        center_t = get_height / 2
     }
-    let center_l = get_width / 2;
+    let center_l = get_width / 2
     let get_sock = $('.pong').width() / 2
     center_l = center_l - get_sock
     $('.pong').css('top', `${center_t}px`)
@@ -59,7 +59,7 @@ set_position()
 
 $('.football').bind('click', function (e) {
 
-    //var offset = $('.football').offset();
+    //var offset = $('.football').offset()
 
     // why 100? (half by 200px) (200px = image ball)
 
@@ -123,3 +123,72 @@ $('.ex5 ul li').on('click', function() {
 })
 
 /* ex_6 */
+
+// it not ideal variant, but ok
+
+let barch = ['block_1', 'block_2']
+
+for(let i = 0; i < barch.length; i++) {
+    $(`.ex6 .${barch[i]} button`).hover(function() {
+        let get_block_top = $(`.ex6 .${barch[i]}`).offset().top
+        let get_button = $(`.ex6 .${barch[i]} button`).offset().top
+        console.log(get_block_top)
+        console.log(get_button)
+        // 40 height
+        let get_margin_top = 0
+        get_margin_top = (get_block_top+50 >= get_button) ? 26 : -32
+        $(`.ex6 .${barch[i]} button`).before(`<div class="tooltip" style="margin-left: 0px; margin-top: ${get_margin_top}px;">Tool tip ${i+1}</div>`)
+    }, function() {
+        $('.tooltip').remove()
+    })
+}
+
+/* ex_7 */
+
+$('.ex7 ul ul li').on('click', function() {
+    $(this).find('ul').slideToggle();
+})
+
+$('.ex7 ul ul li').hover(function() {
+    $(this).css('font-weight', '600')
+    $(this).find('ul').css('font-weight', '400')
+}, function() {
+    $(this).css('font-weight', '400')
+    $(this).find('ul').css('font-weight', '400')
+})
+
+/* ex_8 */
+
+// https://stackoverflow.com/questions/8960193/how-to-make-html-element-resizable-using-pure-javascript
+
+var p = document.getElementById('ex8')
+
+p.addEventListener('click', function init() {
+    p.removeEventListener('click', init, false);
+    p.className = p.className + 'resizable';
+    var resizer = document.createElement('div');
+    resizer.className = 'resizer';
+    p.appendChild(resizer);
+    resizer.addEventListener('mousedown', initDrag, false);
+}, false);
+
+var startX, startY, startWidth, startHeight;
+
+function initDrag(e) {
+   startX = e.clientX;
+   startY = e.clientY;
+   startWidth = parseInt(document.defaultView.getComputedStyle(p).width, 10);
+   startHeight = parseInt(document.defaultView.getComputedStyle(p).height, 10);
+   document.documentElement.addEventListener('mousemove', doDrag, false);
+   document.documentElement.addEventListener('mouseup', stopDrag, false);
+}
+
+function doDrag(e) {
+   p.style.width = (startWidth + e.clientX - startX) + 'px';
+   p.style.height = (startHeight + e.clientY - startY) + 'px';
+}
+
+function stopDrag(e) {
+    document.documentElement.removeEventListener('mousemove', doDrag, false);
+    document.documentElement.removeEventListener('mouseup', stopDrag, false);
+}
